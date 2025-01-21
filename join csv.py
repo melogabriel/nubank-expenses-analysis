4d4be5e3fb07b138e553a40796e28b385cea782b
Gabriel Melo <46683068+melogabriel@users.noreply.github.com>
2025-01-21 19:43:47 -0300

    Add files via upload

---
+++ join csv.py
@@ -0,0 +1,12 @@
+import os
+import glob
+import pandas as pd
+os.chdir("/Users/Gabriel/Desktop/Faturas nubank")
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+#combinar todos os arquivos da lista
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+#exportar para csv
+combined_csv.to_csv( "faturas nubank.csv", index=False, encoding='utf-8-sig')
